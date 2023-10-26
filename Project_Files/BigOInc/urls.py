12ec65e60d8333789d7f1c6551dd156ea837dbdd
Loc N <101013858+Skyhorizon2021@users.noreply.github.com>
2023-10-25 23:42:40 -0700

    Update urls.py

--- Project_Files/BigOInc/urls.py
+++ Project_Files/BigOInc/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("/FinderKeeper/", include("FinderKeeper.urls")),
+    path("FinderKeeper/", include("FinderKeeper.urls")),
     path('admin/', admin.site.urls),
 ]
